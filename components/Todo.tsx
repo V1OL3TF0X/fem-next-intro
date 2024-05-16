02afb41c1c9082dc96c820b1adf0b85776260f80
Mateusz Adamiec <mateusz.adamiec@a4bee.com>
2024-05-16 08:48:00 +0200

    completing todos

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,12 +1,19 @@
+'use client';
+import { completeTodo } from '@/db';
 import { Prisma } from '@prisma/client';
+import { useTransition } from 'react';
 const Todo = <Element extends React.ElementType = 'div'>({
   todo,
   as: El = 'div',
   ...rest
-}: { todo: Prisma.TodoSelect; as?: Element } & React.ComponentPropsWithRef<Element>) => (
-  <El {...rest}>
-    <input type='checkbox' checked={todo.completed} /> {todo.content}
-  </El>
-);
+}: { todo: Prisma.TodoSelect; as?: Element } & React.ComponentPropsWithRef<Element>) => {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <El {...rest}>
+      <input type='checkbox' checked={todo.completed} onClick={() => startTransition(() => completeTodo(todo.id))} />{' '}
+      <span className={todo.completed ? 'text-gray-600 line-through ' : ''}>{todo.content}</span>
+    </El>
+  );
+};
 
 export default Todo;
